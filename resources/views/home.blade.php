7962c917ec95fe34849a768dd941b1fb1be4367c
Ignasi Bosch <ibosch@afilias.info>
2016-06-03 14:17:43 +0100

    Added refresh button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,62 +1,76 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Codette</title>
+<head>
+    <title>Codette</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <style>
+        html, body {
+            height: 100%;
+        }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-            }
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+        }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
 
-            .title {
-                font-size: 96px;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+        .title {
+            font-size: 96px;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
 
-            .problem {
-                color: black !important;
-                font-size: 153%;
-                margin: auto;
-                padding-top: 2%;
-                padding-bottom: 5%;
-                text-align: left;
-                width: 75%;
-                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-            }
+        .problem {
+            color: black !important;
+            font-size: 153%;
+            margin: auto;
+            padding-top: 2%;
+            padding-bottom: 5%;
+            text-align: left;
+            width: 75%;
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        }
 
-            a:link, a:active, a:visited, a:hover{
-                color: black;
-                text-decoration: none;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">{{ $language }}</div>
-                <div class="problem"><a href="{{ $url  }}" target="_blank">({{ $randomProblemNumber  }})</a>  {!! $problem !!} </div>
-            </div>
+        a:link, a:active, a:visited, a:hover {
+            color: black;
+            text-decoration: none;
+        }
+
+        .btn {
+            margin: 0 0 20px;
+            border: none;
+            padding: 5px;
+            cursor: pointer;
+            color: #d2d2d2;
+            background-color: #606060;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">{{ $language }}</div>
+        <div class="problem">
+            <button class="btn" onclick="javascript:location.reload()">Change Challenge</button>
+            <br>
+            <a href="{{ $url  }}" target="_blank">({{ $randomProblemNumber  }})</a>
+            {!! $problem !!}
         </div>
-    </body>
+    </div>
+</div>
+</body>
 </html>
